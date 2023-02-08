411351c03e134def14ccf2f3a83f1b279747f614
Ken Chang <63435519+kenchang8787@users.noreply.github.com>
2023-02-08 16:51:48 +0800

    data types

---
+++ reference_types/src/main/java/kenchang8787/com/Main.java
@@ -0,0 +1,45 @@
+package kenchang8787.com;
+
+public class Main {
+  public static void main(String[] args) {
+
+    primitiveTypes();
+    referenceTypes();
+  }
+
+  /** to print the primitive data types */
+  private static void primitiveTypes() {
+    // primitive data types
+    boolean a = false;
+    byte b = 127;
+    short c = 32767;
+    char d = '\u4fe5';
+    int e = 2147483647;
+    long f = 822337766;
+    float g = 5.75f;
+    double h = -3.25;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(g);
+    System.out.println(h);
+  }
+
+  /** to print the reference data types */
+  private static void referenceTypes() {
+
+    String a = "AA";
+    Integer b = 323;
+    Float c = 1.25F;
+    Double d = 0.22;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+  }
+}

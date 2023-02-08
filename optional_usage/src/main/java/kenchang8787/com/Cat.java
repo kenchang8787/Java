aee87a7daae1d05ba3f770cc784742d46899d9eb
Ken Chang <63435519+kenchang8787@users.noreply.github.com>
2023-02-08 13:35:44 +0800

    optional_usage

---
+++ optional_usage/src/main/java/kenchang8787/com/Cat.java
@@ -0,0 +1,16 @@
+package kenchang8787.com;
+
+import lombok.Data;
+
+@Data
+public class Cat {
+
+  private String name;
+
+  private int age;
+
+  public Cat(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+}
